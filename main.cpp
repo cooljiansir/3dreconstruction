54cac69726fdc621509b4492067b007abfa6cc02
zhijian <cooljiansir@gmail.com>
2014-04-27 00:00:38 +0800

    求梯度，求卷积(未测试)

--- main.cpp
+++ main.cpp
@@ -54,6 +54,140 @@ void moravec(Mat &img,int thre){
 }
 
 
+void gradient_x(Mat &imggray,Mat &res){
+    Size size = imggray.size();
+    res.create(size,CV_64F);
+
+    unsigned char *imgptr = imggray.data;
+    double *resptr = (double *)res.data;
+
+    //initial border
+    for(int i = 0;i<size.height;i++)
+        resptr[i*size.width] = resptr[(i+1)*size.width-1] = 0;
+    for(int i = 0;i<size.height;i++){
+        double *resptri = resptr + i*size.width;
+        unsigned char *imgptri = imgptr + i*size.width;
+        for(int j = 1;j+1<size.width;j++){
+            resptri[j] = imgptri[j+1] - imgptri[j-1];
+        }
+    }
+}
+void gradient_x_3(Mat &imggray,Mat &res,int arg=1){
+    Size size = imggray.size();
+    res.create(size,CV_64F);
+
+    unsigned char *imgptr = imggray.data;
+    double *resptr = (double *)res.data;
+
+    //initial border
+    for(int i = 0;i<size.height;i++)
+        resptr[i*size.width] = resptr[(i+1)*size.width-1] = 0;
+    for(int i = 0;i<size.width;i++)
+        resptr[i] = resptr[(size.height-1)*size.width+i] = 0;
+    for(int i = 1;i+1<size.height;i++){
+        double *resptri = resptr + i*size.width;
+        unsigned char *imgptri = imgptr + i*size.width;
+        unsigned char *imgptri_p = imgptri - size.width;
+        unsigned char *imgptri_n = imgptri + size.width;
+        for(int j = 1;j+1<size.width;j++){
+            resptri[j] = imgptri[j+1] - imgptri[j-1]
+                    +arg*(imgptri_n[j+1] - imgptri_n[j-1])
+                    +imgptri_p[j+1] - imgptri_p[j-1];
+        }
+    }
+}
+void gradient_y(Mat &imggray, Mat &res){
+    Size size = imggray.size();
+    res.create(size,CV_64F);
+
+    double * resptr = (double*)res.data;
+    unsigned char *imgptr = imggray.data;
+
+    //initial border
+    for(int i = 0;i<size.width;i++)
+        resptr[i] = resptr[(size.height-1)*size.width+i] = 0;
+
+    for(int i = 1;i+1<size.height;i++){
+        double *resptri = resptr + i*size.width;
+        unsigned char *imgptri = imgptr + i*size.width;
+        unsigned char *imgptri_p =imgptri - size.width;
+        unsigned char *imgptri_n =imgptri + size.width;
+        for(int j = 0;j<size.width;j++){
+            resptri[j] = imgptri_n[j] - imgptri_p[j];
+        }
+    }
+}
+void gradient_y_3(Mat &imggray, Mat &res,int arg=1){
+    Size size = imggray.size();
+    res.create(size,CV_64F);
+
+    double * resptr = (double*)res.data;
+    unsigned char *imgptr = imggray.data;
+
+    //initial border
+    for(int i = 0;i<size.width;i++)
+        resptr[i] = resptr[(size.height-1)*size.width+i] = 0;
+    for(int i = 0;i<size.height;i++)
+        resptr[i*size.width] = resptr[(i+1)*size.width-1] = 0;
+
+    for(int i = 1;i+1<size.height;i++){
+        double *resptri = resptr + i*size.width;
+        unsigned char *imgptri = imgptr + i*size.width;
+        unsigned char *imgptri_p =imgptri - size.width;
+        unsigned char *imgptri_n =imgptri + size.width;
+        for(int j = 0;j<size.width;j++){
+            resptri[j] =imgptri_n[j-1] - imgptri_p[j-1]
+                        +arg*(imgptri_n[j] - imgptri_p[j])
+                        +imgptri_n[j+1] - imgptri_p[j+1];
+        }
+    }
+}
+
+void conv(Mat &mata,Mat &matb,Mat &res){
+    //高斯滤波窗口
+    double gauss[5*5] = {
+                        1.0 / 273,	4.0  / 273,	7.0  / 273,	4.0  / 273,	1.0/273,
+                        4.0 / 273,	16.0 / 273,	26.0 / 273,	16.0 / 273,	4.0/273,
+                        7.0 / 273,	26.0 / 273,	41.0 / 273,	26.0 / 273,	7.0/273,
+                        4.0 / 273,	16.0 / 273,	26.0 / 273,	16.0 / 273,	4.0/273,
+                        1.0 / 273,	4.0  / 273,	7.0  / 273,	4.0  / 273,	1.0/273};
+    if(mata.size()!=matb.size())
+        return;
+    Size size = mata.size();
+
+    res.create(size,CV_64F);
+    double *resptr = (double *)res.data;
+    double *mataptr = (double *)mata.data;
+    double *matbptr = (double *)matb.data;
+
+    //initial border
+    for(int i = 0;i<2;i++)
+        for(int j = 0;j<size.width;j++)
+            resptr[i*size.width+j] = resptr[(size.height-1-i)*size.width+j] = 0;
+    for(int i = 0;i<size.height;i++)
+        for(int j = 0;j<2;j++)
+            resptr[i*size.width+j] = resptr[i*size.width+size.width-1-j] = 0;
+
+    //begin
+    for(int i = 2;i+2<size.height;i++){
+        double *mataptri = mataptr + i * size.width;
+        double *matbptri = matbptr + i * size.width;
+        double *resptri = resptr + i * size.width;
+        for(int j = 2;j+2<size.width;j++){
+            double sum = 0;
+            int windex = 0;
+            for(int i1 = -2;i1<=2;i1++)
+                for(int j1 = -2;j1<=2;j1++){
+                    sum += gauss[windex]
+                            * mataptri[i1*size.width+j+j1]
+                            * matbptri[i1*size.width+j+j1];
+                    windex++;
+                }
+            resptri[j] = sum;
+        }
+    }
+}
+
 void testMoravec(){
     QString filename = QFileDialog::getOpenFileName();
     if(!filename.isEmpty()){
@@ -64,11 +198,38 @@ void testMoravec(){
     }
 }
 
+void testgradient(){
+    QString filename = QFileDialog::getOpenFileName();
+    if(!filename.isEmpty()){
+        Mat img = imread(filename.toStdString().c_str());
+        Mat imggray;
+        cvtColor(img,imggray,CV_BGR2GRAY);
+        Mat gx,gx_3,gy,gy_3;
+
+        gradient_x(imggray,gx);
+        gradient_x_3(imggray,gx_3);
+        gradient_y(imggray,gy);
+        gradient_y_3(imggray,gy_3);
+
+        Mat disgx,disgx_3,disgy,disgy_3;
+        gx.convertTo(disgx,CV_8U);
+        gx_3.convertTo(disgx_3,CV_8U);
+        gy.convertTo(disgy,CV_8U);
+        gy_3.convertTo(disgy_3,CV_8U);
+
+        imshow("gx",disgx);
+        imshow("gx_3",disgx_3);
+        imshow("gy",disgy);
+        imshow("gy_3",disgy_3);
+
+    }
+}
 
 int main(int argc,char * argv[]){
     QApplication app(argc,argv);
 
-    testMoravec();
+//    testMoravec();
+    testgradient();
     cvWaitKey(0);
 
     return app.exec();
